498b9b3ae428565b7585192f2a6f160c6e6a1c4b
RamitShapkota <shapkotaramit47@gmail.com>
2026-06-24 08:33:45 +0545

    Add naming conventions and example variable declarations

--- simple.py
+++ simple.py
@@ -1,1 +1,18 @@
-print("hello i am ramit")
+# Naming convension
+
+camelCase = "firstName , lastName"
+PascalCase = "FirstName , FastName" #usefor class name
+snake_case = "first_name, last_name" #use for variable
+
+# constant dec in python 
+Name = "ramit"
+
+boolean = True, False , None
+
+print(Name.upper())
+
+list1 = [10, 20, "ram"]
+print(list1)
+
+
+
